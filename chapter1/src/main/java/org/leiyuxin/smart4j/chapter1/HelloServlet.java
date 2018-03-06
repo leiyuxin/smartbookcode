c54de4c58085edce7c8269afb4ebbd2fbec8ffaf
leiyuxin <617705033@qq.com>
2018-03-07 02:44:59 +0800

    chapter2study
    Change-Id: If1cbceb50515188a3252f8029511a41c26583e34
    Signed-off-by: leiyuxin <617705033@qq.com>

--- chapter1/src/main/java/org/leiyuxin/smart4j/chapter1/HelloServlet.java
+++ chapter1/src/main/java/org/leiyuxin/smart4j/chapter1/HelloServlet.java
@@ -10,14 +10,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String currentTime = dateFormat.format(new Date());
-        req.setAttribute("currentTime", currentTime);
-        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req, resp);
-    }
+	private static final Logger LOGGER = LoggerFactory.getLogger(HelloServlet.class);
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String currentTime = dateFormat.format(new Date());
+		req.setAttribute("currentTime", currentTime);
+		try {
+			req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req, resp);
+		} catch (Exception e) {
+			LOGGER.error("Exception 发生", e);
+		}
+
+	}
 }
